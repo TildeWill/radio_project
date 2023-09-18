7c1c4b2715ca3beddcffd89bcc47942950370566
Will Read <will.read@gmail.com>
2023-09-18 11:43:16 -0700

    beginning of scrollable sprite work

---
+++ RadioProject/ScrollableSprite.h
@@ -0,0 +1,14 @@
+#ifndef ScrollableSprite_h
+#define ScrollableSprite_h
+#include "Arduino.h" 
+#include <TFT_eSPI.h>
+
+class ScrollableSprite {
+public:
+  ScrollableSprite();
+  
+private:
+  
+};
+
+#endif
